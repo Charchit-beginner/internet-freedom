5ebeb02aaf3fbb9b15dd1b4fbbb195d0d8220d77
charchit-beginner <charchit.dahiya@gmail.com>
2021-08-25 00:21:38 +0530

    fixed upper case chatbot issue

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.onload = () => {
-	var q = ["hi","hey","hello","whorichrdstllmn","whojullinssnge","whoedwrdsnowden"]
+    var q = ["hi","hey","hello","whorichrdstllmn","whojullinssnge","whoedwrdsnowden"]
 var re = ["hi","hello","what's up","Richard Stallman is an American Activist and Programmer. He was born on March 16 ,1953. He got interested in computers in a very young age. He used to read manuals of IBM 7094.","Julian was born on 3 July 1971. He studied programming, mathematics, and physics at Central Queensland University in 1994. In 1993, he was involved in starting one of the first public internet service providers in Australia.","Edward was born on June 21, 1983. He is a computer intelligence consultant who leaked highly classified information from the National Security Agency in 2013 when he worked for the Central Intelligence Agency"]
     AOS.init();
     var chat_pop = document.querySelector(".chat-btn")
@@ -20,8 +20,8 @@ var re = ["hi","hello","what's up","Richard Stallman is an American Activist and
     send.addEventListener("click", () => {
 
         console.log(UserInput.value == "/help")
-    	if (UserInput.value != ""){
-    	var a = q.indexOf( UserInput.value.replace(/\s|a|me|please|can|you|is|ifeel|are|/g, ''))
+        if (UserInput.value != ""){
+        var a = q.indexOf( UserInput.value.toLowerCase().replace(/\s|a|me|please|can|you|is|ifeel|are|/g, ''))
         append("right", UserInput.value)
         if (UserInput.value == "/help"){
             append("left","You can ask the following - hi,hello,hey,who is richard stallman,who is jullian assange","who is edward snowden")
@@ -33,7 +33,7 @@ var re = ["hi","hello","what's up","Richard Stallman is an American Activist and
         }
          
         else {
-        	append("left","Sorry, I am afraid , I don't understand you. type /help to get help")
+            append("left","Sorry, I am afraid , I don't understand you. type /help to get help")
 
         }
                 stat.innerText = "Online";
@@ -42,14 +42,14 @@ var re = ["hi","hello","what's up","Richard Stallman is an American Activist and
     }
     })
     document.addEventListener("scroll",()=>{
-    	if (!window.scrollY == 0){
+        if (!window.scrollY == 0){
 
-    	document.querySelector(".navbar").classList.add("bg-dark")
-    	}
-    	else{
-    	document.querySelector(".navbar").classList.remove("bg-dark")
+        document.querySelector(".navbar").classList.add("bg-dark")
+        }
+        else{
+        document.querySelector(".navbar").classList.remove("bg-dark")
 
-    	}
+        }
     })
     try{
         var typed = new Typed(".element", {
@@ -66,7 +66,6 @@ var re = ["hi","hello","what's up","Richard Stallman is an American Activist and
     catch{
     console.log("e")      
     }
-    
 }
 
 function append(pos, msg) {
